0182f94cdab75101cda4e54fcfb19b48d2af15cb
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-05-09 17:33:16 -0500

    adsasd

--- src/Component/global/ListarComprobantes.js
+++ src/Component/global/ListarComprobantes.js
@@ -414,7 +414,7 @@ class ListarComponentes extends Component {
                             </td>
                             <td  className={boxClass.join(' ')}>
                                 <button id={dynamicData.observacion_upg} name={dynamicData.id_rec}
-                                    onClick={(e) => this.openModalAsignar()} className="btn btn-success">
+                                    onClick={() => this.openModalAsignar()} className="btn btn-success">
                                     Asignar
                                 </button>
                                 <button id={dynamicData.observacion_upg} name={dynamicData.id_rec}
