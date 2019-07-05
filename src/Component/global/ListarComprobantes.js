ac801b8f671667d48398fd2fb733b909c45227fb
Nahum Flores Gutierrez <36179329+NahumF@users.noreply.github.com>
2019-07-04 20:20:43 -0500

    test2

--- src/Component/global/ListarComprobantes.js
+++ src/Component/global/ListarComprobantes.js
@@ -6,16 +6,11 @@ import Combo from './Combo';
 import URL from './API/API';
 import Check from './Check';
 import MyModalUpg from './MyModalUpg';
-import Listardatos from './ListarComprobantes';
 import './css/DatosCSS.css';
 import './css/bootstrap.css';
 import './css/bootstrap.min.css';
 //import Datos from './Datos/Items';
-import swal from 'sweetalert';
-import ModalAsignar from './ModalAsignar';
 
-import {FaCheck} from 'react-icons/fa';
-import {FaWindowClose} from 'react-icons/fa';
 
 class ListarComponentes extends Component {
     constructor(...props) {
@@ -32,86 +27,25 @@ class ListarComponentes extends Component {
         this.verificar = this.verificar.bind(this);
         this.groupBy = this.groupBy.bind(this);
         this.eventoNombre = this.eventoNombre.bind(this);
-
-        //Cambios
-        this.asignar_desasignar = this.asignar_desasignar.bind(this);
-        this.desasignarAlumno = this.desasignarAlumno.bind(this);
-
-        //JDLC ADD => FUNCTION
-        this.updateTable = this.updateTable.bind(this);
-
         this.state = {
             data: null,
             dataOrdenada: null,
             ubicDato: [],
             JSON: [],
-            isLoading: false,
-
-            // oculto:true,
-            addClass     : false,
-
-            //JDLC ADD => STATE
-            rerender     : false
+            isLoading: false
         }
-    }
-
-    //JDLC ADD => DIDUPDATE LIFECICLE
-    componentDidUpdate(prevProps, prevState){
-        
-        if(prevState.rerender !== this.state.rerender){
-
-            /*MY NAME IS BIG BOY */
-            let url = URL.url.concat('recaudaciones/detallada/');
-            let arra = {
-               "nombre"     : this.props.nombreUpdate,
-               "periodoI"   : this.props.periodoIUpdate,
-               "id_concepto": this.props.conceptoUpdate,
-               "periodoF"   : this.props.periodoFUpdate,
-               "voucher"    : this.props.voucherUpdate,
-               "dni"        : this.props.dniUpdate,
-               "codigo"     : this.codigoUpdate
-           };
-
-           fetch(url, {
-               method: 'POST',
-               headers: {
-                   'Accept': 'application/json',
-                   'Content-Type': 'application/json',
-               },
-               body: JSON.stringify(arra, null, 2)
-           })
-               .then((response) => {
-                   console.log(response);
-                   return response.json()
-               })
-               .then(responseJson => {
-                   console.log(responseJson);
-                   let orderedList = responseJson.data.sort((a,b) => {
-                    if (a.nombre > b.nombre) return 1;
-                    
-                    if (a.nombre < b.nombre) return -1;
-                    
-                    return 0;
-                   });
-
-                   this.setState({ data: orderedList });
-               });
-            /*END BIG BOY */
-        }
-    }
-
-    updateTable(){
-        this.setState({rerender: !this.state.rerender});
     }
 
     componentWillMount() {
         let arreglo = [];
-        const lista = this.props.listado; //Lo pasa de content.js
+        const lista = this.props.listado;
+        console.log("Esta es la lista");
+        console.log(lista);
         if (lista !== null) {
             lista.map((item, key) => {
                 arreglo = arreglo.concat(new this.Obj(item.id_rec, item.observacion, item.observacion_upg, item.id_ubicacion
                     && item.id_ubicacion, item.tipo, item.validado, item.nombre,
-                    item.concepto, item.descripcion_min, item.sigla_programa, item.id_programa,
+                    item.concepto, item.descripcion_min, item.nombre_programa, item.id_programa, item.sigla_programa,
                     item.id_registro, item.codigo, item.dni, item.recibo, item.moneda, item.mascara,
                     item.importe, item.fecha, item.id_alum
                 ));
@@ -196,7 +130,7 @@ class ListarComponentes extends Component {
     }
 
     //crea un objeto para pasar al hijo
-    Obj(id_rec, obs, obs_upg, ubic, tipo, validado, nombre, concepto, descripcion_min, sigla_programa, id_programa, id_registro, codigo, dni, recibo,
+    Obj(id_rec, obs, obs_upg, ubic, tipo, validado, nombre, concepto, descripcion_min, nombre_programa, id_programa, sigla_programa, id_registro, codigo, dni, recibo,
         moneda, mascara, importe, fecha, id_alum) {
         this.id_rec = id_rec;
         this.obs = obs;
@@ -207,11 +141,12 @@ class ListarComponentes extends Component {
         this.nombre = nombre;
         this.concepto = concepto;
         this.descripcion_min = descripcion_min;
-        this.sigla_programa = sigla_programa;
+        this.nombre_programa = nombre_programa;
         this.id_programa = id_programa;
+        this.sigla_programa = sigla_programa;
         this.id_registro = id_registro;
         this.codigo = codigo;
-		this.dni = dni;
+        this.dni = dni;
         this.recibo = recibo;
         this.moneda = moneda;
         this.mascara = mascara;
@@ -302,160 +237,9 @@ class ListarComponentes extends Component {
         ReactDOM.render(component, node);
 
     }
-
-    // ===========================================
-    // MODAL PARA ASIGNAR
-    // ============================================
-
-    //Desasignación de la vista principal Asignar/Desasignar
-    desasignarAlumno(programa,sigla_programa,codigoAlu) {
-
-        console.log(programa);
-        console.log(sigla_programa);
-        console.log(codigoAlu);
-
-
-        if(programa == "" || programa == null || codigoAlu == "" || codigoAlu==null){
-            swal("Alumno(a) desasignado(a)","El(La) alumno(a) ya se encuentra desasignado(a)","error");
-        }else{
-              swal("Confirmar","¿Esta seguro(a) de desasignar el(la) alumno(a)?",{ icon: "warning",
-        buttons: ['Cancelar', 'Confirmar'],
-        closeOnClickOutside: false})
-        .then((confirm)=>{
-            if(confirm){
-                let url = URL.url.concat("desasignarRecibo");
-
-        
-                try {
-                    let codigoAlumno=this.state.alumno[0].codigo;
-                    let idAlumno=this.state.alumno[0];
-                    //Variable recibo
-                    let numRecibo=this.state.alumno[0].numero_recibo;
-
-                fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({numRecibo:numRecibo})
-                }).then(res => res.json())
-                    .then(res => {
-                        console.log(res);
-                        if(res.status==='success'){
-                            /*
-                            /////////////////////////
-                            Al agregar, se tiene q actualizar automáticamente
-                            
-                            //Error
-                            this.data[0].codigo = "312312";
-                            this.data[0].id_programa = "4323232";
-                            */
-                            
-                            swal("Alumno desasignado","El alumno fue desasignado exitosamente",{ icon:"success",
-                            closeOnClickOutside: false})
-                            .then((desasigned)=>{
-                                if(desasigned){
-                                    // this.Listardatos.setState({data:[]});
-                                }
-                            });
-                        }else{
-                            swal("Alumno(a) no desasignado(a)","El(La) alumno(a) no pudo ser desasignado(a)","error");
-                        }
-                        this.updateTable();
-                    });
-                } catch (error) {
-                    swal("Alumno(a) desasignado(a)","El(La) alumno(a) ya se encuentra desasignado(a)","error");
-
-                    console.log("Error de asignación de datos: ", error);
-                }
-            }
-        });
-        }
-      
-    }
-   
-        asignar_desasignar(numRecibo,codigoAlu,programa,tipo,id_alum,nombreCompleto, fechaRecibo,sigla_programa) 
-        {
-
-        console.log("*********", sigla_programa);
-        let url = URL.url.concat("programas");
-        console.log(url);
-
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        }).then(res => res.json())
-            .then(res => {
-                console.log(res);
-                if (res.status) {
-                    this.setState({
-                        programas: res.data
-                    });
-
-                    let url1 = URL.url.concat("alumnos/" + numRecibo);
-                    console.log(url1);
-                    fetch(url1, {
-                        method: 'GET',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json',
-                        }
-                    }).then(res => res.json())
-                        .then(res => {
-                            console.log(res);
-                            if (res.status) {
-                                this.setState({
-                                    alumno: res.data
-                                });
-                                console.log(this.state);
-
-                                if(tipo===1){
-                                    let component = <ModalAsignar 
-                                    codigoAlu={codigoAlu}
-                                    id_programa={programa?programa:8} 
-                                    recibo={numRecibo} 
-                                    nombre = {nombreCompleto} 
-                                    fecha = {fechaRecibo}
-                                    id_alum={id_alum} 
-                                    siglaPrograma = {sigla_programa}
-                                    alumno={this.state.alumno} 
-                                    programas={this.state.programas}
-                                    estado={true} 
-                                     
-                                    // JDLC ADD => Funtion trigger as prop
-                                    triggerTableUpdate = {this.updateTable}
-                                     />;
-                                    let node = document.createElement('div');
-                                    ReactDOM.render(component, node);
-                                }else{
-                                    if(tipo===2){
-                                      this.desasignarAlumno(programa,sigla_programa,codigoAlu);
-                                    }
-                                }
-                            } else {
-                                console.log("error");
-                            }
-                        });
-                } else {
-                    console.log("error");
-                }
-            });
-            console.log(this.state);
-    }
-
-    // ===========================================
-    // MODAL PARA ASIGNAR
-    // ============================================
-
-
-
     openModalUpg(e) {
         let id = e;
-        const url = 'https://back-demo-sigap.herokuapp.com/recaudaciones/observaciones/' + id;
+        const url = 'https://modulocontrol.herokuapp.com/recaudaciones/observaciones/' + id;
         //console.log(url);
         fetch(url, {
             method: 'GET',
@@ -601,10 +385,8 @@ class ListarComponentes extends Component {
                             <th>Ubicación</th>
                             <th>Verificar</th>
                             <th>Observaciones</th>
-                            <th className={this.state.expand ? "" : "d-none"}>Asignar/Desasignar</th>
                             <th className={this.state.expand ? "" : "d-none"}>Cuenta del Banco</th>
                             <th className={this.state.expand ? "" : "d-none"}>Tipo de Carga</th>
-                            
                         </tr>
                     </thead>
                     <tbody>{listado.map((dynamicData, i) =>
@@ -618,7 +400,7 @@ class ListarComponentes extends Component {
                             <td>{dynamicData.recibo}</td>
                             <td>{dynamicData.moneda}</td>
                             <td>{dynamicData.mascara} {dynamicData.importe}</td>
-                            <td>{dynamicData.fecha.substring(0,10)}</td>
+                            <td>{dynamicData.fecha}</td>
                             <td>
                                 <Combo
                                     items={this.state.ubicDato}
@@ -627,10 +409,6 @@ class ListarComponentes extends Component {
                                     id_rec={dynamicData.id_rec}
                                 />
                             </td>
-
-
-
-
                             <td>
                                 <Check
                                     validado={dynamicData.validado}
@@ -639,40 +417,18 @@ class ListarComponentes extends Component {
                                     disabled={true}
                                 />
                             </td>
-                            
-
-                            <td className="two-obs">
+                            <td className="two-fields">
                                 <button id={dynamicData.observacion} name={dynamicData.id_rec}
                                     onClick={(e) => this.openModal(dynamicData.id_rec, dynamicData.obs)} className="btn btn-primary">
                                     <span className="mybtn-red glyphicon glyphicon-eye-open"></span>
                                 </button>
                                 <button id={dynamicData.observacion} name={dynamicData.id_rec}
-                                    onClick={(e) => this.openModalUpg(dynamicData.id_rec, dynamicData.obs)} className="btn btn-primary">
+                                    onClick={(e) => this.openModal(dynamicData.id_rec, dynamicData.obs)} className="btn btn-primary">
                                     <span className="mybtn-blue glyphicon glyphicon-eye-open"></span>
                                 </button>
                             </td>
-
-                            <td className={this.state.expand ? "two-fields" : "d-none two-fields"}>
-                                <button id={dynamicData.observacion_upg} name={dynamicData.id_rec}
-                                    onClick={(e) => this.asignar_desasignar(dynamicData.recibo,dynamicData.codigo,dynamicData.id_programa,1,dynamicData.id_alum,dynamicData.nombre, dynamicData.fecha,dynamicData.sigla_programa)} className="btn btn-success">
-                                    <FaCheck/>
-                                </button>
-                                <button id={dynamicData.observacion_upg} name={dynamicData.id_rec}
-                                    onClick={(e) =>this.asignar_desasignar(dynamicData.recibo,dynamicData.codigo,dynamicData.id_programa,2,dynamicData.id_alum,dynamicData.sigla_programa)} className="btn btn-danger">
-                                    <FaWindowClose/>
-                                </button>
-                            </td>
-
                             <td className={this.state.expand ? "" : "d-none"}>{dynamicData.tipo}</td>
-
-
-                            <td className={this.state.expand ? "" : "d-none"}>{dynamicData.id_registro == 2103 ? "DIGITADO" : "REMITIDO"}</td>                         
-
-
-
-                            
-                            
-
+                            <td className={this.state.expand ? "" : "d-none"}>{dynamicData.id_registro == 2103 ? "DIGITADO" : "REMITIDO"}</td>
                         </tr>
                     )}
                     </tbody>
